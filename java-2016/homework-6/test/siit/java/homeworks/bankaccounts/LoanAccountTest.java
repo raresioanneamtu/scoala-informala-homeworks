02174815f1f10b38b2ee99a65db832e1ed9d58a3
Rares Neamtu <raresioan.neamtu@gmail.com>
2016-05-17 23:45:11 +0300

    Make some changes at the loan.account.test .

--- java-2016/homework-6/test/siit/java/homeworks/bankaccounts/LoanAccountTest.java
+++ java-2016/homework-6/test/siit/java/homeworks/bankaccounts/LoanAccountTest.java
@@ -9,17 +9,10 @@ import org.junit.Test;
 public class LoanAccountTest {
 
 	@Test
-	public void loanAccepted_loanPouredIntoRunningAccount() {
+	public void test() {
 		// given
-		float installmentsPeriod = 60f;
-		float loanAmount = 10000f;
 		// when
-		Date accountCreationTime = TestUtils.getDate(2016, 1, 1);
-		LoanAccount loanAccount = new LoanAccount(accountCreationTime, 10000f, installmentsPeriod, 0.01f);
-		RunningAccount runningAccount = new RunningAccount(accountCreationTime, 10f);
-		runningAccount.addFunds(loanAccount.balance);
 		// then
-		assertEquals(loanAmount, runningAccount.getBalance(accountCreationTime), 0);
 
 	}
 
